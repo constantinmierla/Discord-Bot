d13bdca69271d336b71d201b9aa31864cbb69b58
constantinmierla <64612196+constantinmierla@users.noreply.github.com>
2021-02-13 17:54:50 +0200

    Update luni.js

--- commands/luni.js
+++ commands/luni.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'luni',
     description: "orar",
-    execute(client, message, cmd, args, Discord)
+    execute(message, args, cmd, client, Discord)
     {
       message.channel.send('Sport, Eng, Bio, Mate, Ro, Chimie, Muzica');
     }
