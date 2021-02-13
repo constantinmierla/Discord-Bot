7394f25a90bc7f4763bb3cc356172a605a64c9ba
constantinmierla <64612196+constantinmierla@users.noreply.github.com>
2021-02-13 18:03:43 +0200

    Update ore.js

--- commands/ore.js
+++ commands/ore.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'ore',
     description: "pauze",
-    execute(client, message, args)
+    execute(message, args, cmd, client, Discord)
     {
       message.channel.send('8:00, 8:50, 9:40, 10:30, 11:20, 12:10, 13:00');
     }
